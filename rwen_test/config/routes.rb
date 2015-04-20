9eecc76afcc5ef6096405c8f999b4bf466d48d09
Rwen <rwen@dowelltech.cn>
2015-04-20 12:11:39 +0800

    add code
    add code

--- rwen_test/config/routes.rb
+++ rwen_test/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  get 'test/index', to: 'test#index', as: :test_index
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
